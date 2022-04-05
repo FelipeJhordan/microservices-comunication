c72b255f82e7c63f0b0e06000890789e4b2b5916
felipejhordan <felipejordan.alves@gmail.com>
2022-04-05 13:22:02 -0300

    feat: use port env in asales-api

--- sales-api/app.js
+++ sales-api/app.js
@@ -4,9 +4,9 @@ const app = express()
 
 const env = process.env
 
-const PORT = env.PORT || 8083
+const PORT = env.PORT || 8082
 
-app.get('/api/status', () => {
+app.get('/api/status', (req, res) => {
     return res.status(200).json({
         service: 'Sales-API',
         status: 'up',
